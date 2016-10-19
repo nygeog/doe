fcc8db87f01e9b4f8e2930b2a945401490938600
Daniel M Sheehan <daniel.martin.sheehan@gmail.com>
2016-10-18 22:17:45 -0400

    update add additional drive, as MacBook Pro and iMac Paralles have dif. drive letters

--- tasks/201610_mapping/02-copy-to-windows.py
+++ tasks/201610_mapping/02-copy-to-windows.py
@@ -2,6 +2,7 @@ import shutil
 
 #copy input from GitHub to C: local
 inFile = 'Z:/GitHub/doe/tasks/201610_mapping/data/input/from_doe/y2017_pre_k_directory_map_data.csv'
+inFile = 'Y:/GitHub/doe/tasks/201610_mapping/data/input/from_doe/y2017_pre_k_directory_map_data.csv'
 ouFile = wi+'from_doe/y2017_pre_k_directory_map_data.csv'
 
 shutil.copy2(inFile, ouFile)
